9a8ee6ae95fa1975d148149a5b07071202eb9659
Rmnvich <rmnvich@mail.ru>
2019-03-31 18:26:15 +0300

    Completed register screen

--- MeetUp/app/src/main/java/niceguys/apps/meetup/LoginActivity.kt
+++ MeetUp/app/src/main/java/niceguys/apps/meetup/LoginActivity.kt
@@ -1,8 +1,10 @@
 package niceguys.apps.meetup
 
 import android.databinding.DataBindingUtil
+import android.graphics.Typeface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.method.PasswordTransformationMethod
 import niceguys.apps.meetup.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -12,5 +14,8 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        mBinding.etLoginPassword.typeface = Typeface.SANS_SERIF
+        mBinding.etLoginPassword.transformationMethod = PasswordTransformationMethod()
     }
 }
--- MeetUp/app/src/main/java/niceguys/apps/meetup/RegisterActivity.kt
+++ MeetUp/app/src/main/java/niceguys/apps/meetup/RegisterActivity.kt
@@ -1,8 +1,10 @@
 package niceguys.apps.meetup
 
 import android.databinding.DataBindingUtil
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.method.PasswordTransformationMethod
 import niceguys.apps.meetup.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
@@ -12,5 +14,11 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_register)
+
+        mBinding.etRegisterPassword.typeface = Typeface.SANS_SERIF
+        mBinding.etRegisterPassword.transformationMethod = PasswordTransformationMethod()
+
+        mBinding.etRegisterConfirmPassword.typeface = Typeface.SANS_SERIF
+        mBinding.etRegisterConfirmPassword.transformationMethod = PasswordTransformationMethod()
     }
 }
